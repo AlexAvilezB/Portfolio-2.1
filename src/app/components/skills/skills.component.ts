d91ecc4264e11b3267f398aa3dd270f8fd39fbf3
Alejandro Avilez <aleantonio656@gmail.com>
2022-08-07 01:33:58 -0600

    Skill Section
    Card animation on click, content on the back card, styling cards

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
+  @Input()
+  public flipped1: boolean = false;
+  public flipped2: boolean = false;
+
   constructor() {}
+
+  toggleFlip1() {
+    this.flipped1 = !this.flipped1;
+  }
+
+  toggleFlip2() {
+    this.flipped2 = !this.flipped2;
+  }
 
   ngOnInit(): void {}
 }
