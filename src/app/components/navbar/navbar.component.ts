c7876a947797652da32effa15718b8706d7e7575
Alejandro Avilez <aleantonio656@gmail.com>
2022-09-17 21:26:50 -0600

    Header re design

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public show: boolean = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public showNav() {
+    this.show = !this.show;
   }
-
 }
